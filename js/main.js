49da96c008656a7254dc7d6f9801f9b4f8445f6c
johan <johan.elberg38@gmail.com>
2018-07-04 10:27:31 +0300

    final

--- js/main.js
+++ js/main.js
@@ -9,13 +9,14 @@ var app = new Vue({
 
     methods: {
         addToList(){
-            var id = this.elemList.length + 1 // increment a key inside the ocject
+            var id = this.elemList.length + 1 // increment a key inside the object
             if(this.newtoList.length !== 0){ // prevent to add empty string
                 var toPush = {key: id, elem: this.newtoList, isFav: 0}
                 this.elemList.push(toPush)
                 this.newtoList = '' // empty the input
             }
         },
+
 
         deleteElem(key){
             this.elemList = this.elemList.filter(todo => todo.key !== key)
@@ -37,13 +38,13 @@ var app = new Vue({
             this.deleteDone(key)
          },
 
-         favorite(index){  // turn an element to fav (it will also change add a class)
+        favorite(index){  // turn an element to fav (it will also change add a class)
             if(this.favList[index].isFav == 0){
                 this.$set(this.favList[index], 'isFav', 1)
             } else{
                 this.$set(this.favList[index], 'isFav', 0)
             }
-         }
+        }
     },
 
     computed: {
